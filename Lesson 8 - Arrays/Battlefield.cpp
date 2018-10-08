f969f2ed1474d1fe577c5466eb6291b9a8f35a81
sbendell <43750640+sbendell@users.noreply.github.com>
2018-10-08 15:11:04 +0100

    Implemented ProgressGame

--- Lesson 8 - Arrays/Battlefield.cpp
+++ Lesson 8 - Arrays/Battlefield.cpp
@@ -1,9 +1,11 @@
 #include "stdafx.h"
 #include "Battlefield.h"
+#include <iostream>
 
 
-Battlefield::Battlefield(int width, int height, int shipAmount)
+Battlefield::Battlefield(int width, int height, int shipAmount, int turncount)
 {
+	turnCount = turncount;
 	for (int x = 0; x < width; x++)
 	{
 		for (int y = 0; y < height; y++)
@@ -24,7 +26,39 @@ Battlefield::Battlefield(int width, int height, int shipAmount)
 	}
 }
 
+void Battlefield::ProgressGame() {
+	while (remainingTurns() > 0) {
+		int x;
+		int y;
+
+		cout << "Take a shot! Guess X: \n";
+		cin >> x;
+		cout << "Guess Y: \n";
+		cin >> y;
+
+		for (int i = 0; i < ships.size; i++)
+		{
+			if ((x >= ships[i]->get_ship_position().first && x < ships[i]->get_ship_size().first) &&
+				y >= ships[i]->get_ship_position().second && y < ships[i]->get_ship_size().second) {
+				shotAttempts.push_back(make_pair(make_pair(x, y), '+'));
+				cout << "You hit a ship!\n";
+			}
+			else {
+				shotAttempts.push_back(make_pair(make_pair(x, y), '-'));
+				cout << "You missed!\n";
+			}
+		}
+	}
+}
+
+int Battlefield::remainingTurns() const {
+	return turnCount;
+}
 
 Battlefield::~Battlefield()
 {
+	for (Battleship* ship : ships)
+	{
+		delete ship;
+	}
 }
--- Lesson 8 - Arrays/Battlefield.h
+++ Lesson 8 - Arrays/Battlefield.h
@@ -6,9 +6,14 @@ using namespace std;
 class Battlefield
 {
 public:
-	Battlefield(int width, int height, int shipAmount);
+	Battlefield(int width, int height, int shipAmount, int turncount);
 	~Battlefield();
+	void ProgressGame();
+	int remainingTurns() const;
+	void PrintBattlefield() const;
 private:
 	vector<Battleship*> ships;
+	vector<pair<pair<int, int>, char>> shotAttempts;
+	int turnCount;
 };
 
