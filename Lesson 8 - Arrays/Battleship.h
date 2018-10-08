8f648a925bee449e5147da4a47ac2105234ca852
sbendell <43750640+sbendell@users.noreply.github.com>
2018-10-08 14:06:39 +0100

    Created Battleship Class

---
+++ Lesson 8 - Arrays/Battleship.h
@@ -0,0 +1,11 @@
+#pragma once
+#include <random>
+#include <time.h>
+
+class Battleship
+{
+public:
+	Battleship();
+	~Battleship();
+};
+
--- Lesson 8 - Arrays/Lesson 8 - Arrays.cpp
+++ Lesson 8 - Arrays/Lesson 8 - Arrays.cpp
@@ -3,8 +3,7 @@
 
 #include "stdafx.h"
 #include <iostream>
-#include <random>
-#include <time.h>
+#include "Battleship.h"
 #include <tuple>
 
 using namespace std;
@@ -23,43 +22,6 @@ void Print_Array(char charArr[]){
 	for (int i = 0; i < 11; i++) {
 		cout << charArr[i];
 	}
-}
-
-bool** CreateBattleship() {
-	int shipAlignment = rand() % 2;
-	int shipXPos;
-	int shipYPos;
-
-	bool** battleshipPosition = new bool*[8];
-	for (int i = 0; i < 8; i++)
-	{
-		battleshipPosition[i] = new bool[8];
-		for (int x = 0; x < 8; x++)
-		{
-			battleshipPosition[i][x] = false;
-		}
-	}
-
-	if (shipAlignment == 0) {
-		shipYPos = rand() % 6;
-		bool* yPos1 = battleshipPosition[shipYPos];
-		bool* yPos2 = battleshipPosition[shipYPos + 1];
-		bool* yPos3 = battleshipPosition[shipYPos + 2];
-		shipXPos = rand() % 8;
-		yPos1[shipXPos] = true;
-		yPos2[shipXPos] = true;
-		yPos3[shipXPos] = true;
-	}
-	else {
-		shipYPos = rand() % 8;
-		bool* yPos = battleshipPosition[shipYPos];
-		shipXPos = rand() % 6;
-		yPos[shipXPos] = true;
-		yPos[shipXPos + 1] = true;
-		yPos[shipXPos + 2] = true;
-	}
-
-	return battleshipPosition;
 }
 
 void PrintBattleship(bool** battleship, vector<pair<int, int>> hitLocs, int attempts) {
